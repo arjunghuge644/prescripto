8c06b2d46e74ea5477460dcd890aa9ecaa3d369b
arjunghuge644 <arjunghuge644@gmail.com>
2026-07-14 23:32:34 +0530

    doctor panel added

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -56,16 +56,15 @@ const loginDoctor = async (req, res) => {
 //api to get doctor appointments for doctor panel
 
 const appointmentsDoctor = async (req, res) => {
-    try {
-        const { docId } = req.body
-        const appointments = await appointmentModel.find({ docId })
-        
-        res.json({success:true,appointments})
+  try {
+    const { docId } = req.body;
+    const appointments = await appointmentModel.find({ docId });
 
-    } catch (error) {
-        console.log(error);
-        res.json({ success: false, message: error.message });
-    }
-}
+    res.json({ success: true, appointments });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export {changeAvailability,doctorList,loginDoctor,appointmentsDoctor}
